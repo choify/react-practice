9b2a9e8f712441ce57de2853a2a3be77b78595d5
samuelys <samuelys@naver.com>
2022-09-29 09:55:28 +0900

    Infinite Scroll Example

---
+++ src/App.js
@@ -0,0 +1,8 @@
+//import Example1 from "./components/InfiniteScroll/InfiniteScrollExample1"
+import Example2 from "./components/InfiniteScroll/InfiniteScrollExample2";
+
+function App() {
+  return <Example2 />
+}
+
+export default App;
