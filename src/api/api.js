6d37aacba9782c30b0d31ea8297d06bb13373584
samuelys <samuelys@naver.com>
2023-01-01 12:52:03 +0900

    add code

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+
+const baseUrl = "https://kobis.or.kr/kobisopenapi/webservice/rest";
+const key = "781fff11c74308a4e71979891847aa35";
+
+const dailyBoxOffice = `${baseUrl}/boxoffice/searchDailyBoxOfficeList.json`;
+const searchMoiveList = `${baseUrl}/movie/searchMovieList.json`
+
+const yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+const year = yesterday.getFullYear();
+const month = ('0' + (yesterday.getMonth() + 1)).slice(-2);
+const day = ('0' + yesterday.getDate()).slice(-2);
+const date = year + month + day
+
+
+
+export const getDailyMoives = async () => {
+    const response = await axios.get(`${dailyBoxOffice}?key=${key}&targetDt=${date}`)
+    return response
+}
+
+export const getSearchMoiveList = async () => {
+    const response = await axios.get(`${searchMoiveList}?key=${key}&curPage=1&itemPerPage=100&openStartDt=2022`)
+    return response
+}
